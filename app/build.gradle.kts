87865d4f972f8dd24a741f1b910d6a3a5c5e461d
KafuuNeko <kafuuneko@gmail.com>
2026-08-19 16:12:22 +0800

    feat: 配置调试构建独立包名与应用名称

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+        }
         release {
             isMinifyEnabled = false
             if (hasReleaseSigning) {
